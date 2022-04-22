6f9b28c327d6112464fb7e3cc57a217d05e43078
Kimleng-Hong <hong.kimleng.2018@rupp.edu.kh>
2022-04-22 07:44:52 +0700

    Adding stuff

--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,10 @@ Route::get('/edit-customer/{id}', [CustomerController::class, 'edit']);
 Route::put('update-customer/{id}', [CustomerController::class, 'update']);
 
 //Ticket Route
-Route::get('/ticket-list', [TicketController::class, 'index']);
+Route::get('/list-ticket/{id}', [TicketController::class, 'index']);
+Route::get('/create-ticket/{id}', [TicketController::class, 'create']);
+Route::post('store-ticket/{id}', [TicketController::class, 'store']);
+Route::get('/show-ticket/{id}', [TicketController::class, 'show']);
+Route::get('/edit-ticket/{id}', [TicketController::class, 'edit']);
+Route::put('update-ticket/{id}', [TicketController::class, 'update']);
 
